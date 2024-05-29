63616de32508b1fe6c1f573a3fa5136a250a33d3
anjali <anjali.c@codelynks.com>
2024-05-29 10:45:48 +0530

    created doctor and patient model

--- src/contrib/models/doctors.py
+++ src/contrib/models/doctors.py
@@ -1,2 +1,16 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
+class Doctor(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    specialization = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=50)
+    Phone = PhoneNumberField(null=False, blank=False, unique=True)
+
+
+class DoctorSchedule(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='schedules')
+    working_days_of_week = models.CharField(max_length=10)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
