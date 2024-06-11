bd86cb29058a79bffedaea09e831f863346ff538
ivin.s <ivin.s@codelynks.com>
2024-06-11 17:16:45 +0530

    adds schema

--- src/contrib/apis.py
+++ src/contrib/apis.py
@@ -1,4 +1,5 @@
 # ninja
+from datetime import datetime
 from typing import List
 from ninja import NinjaAPI, Schema
 
@@ -17,24 +18,35 @@ class DoctorViewSet(viewsets.ModelViewSet):
         queryset = Doctor.objects.prefetch_related('schedules')
         specialization = self.request.query_params.get('specialization') or None
         if not specialization == None:
-            queryset =queryset.filter(specialization=specialization)
+            queryset = queryset.filter(specialization=specialization)
         return queryset
 
 
 # drf
 api = NinjaAPI()
+
+
 class DoctorScheme(Schema):
     id: int
     name: str
     email: str
     specialization: str
     license_number: str
+    schedules: List["DoctorScheduleScheme"]
     # Phone: str
 
 
+class DoctorScheduleScheme(Schema):
+    id: int
+    # doctor: DoctorScheme
+    working_days_of_week: str
+    start_time: datetime
+    end_time: datetime
+
+
 @api.get("doctors/", response=List[DoctorScheme])
-def doctors(request, specialization: str=None):
-    queryset = Doctor.objects.all()
+def doctors(request, specialization: str = None):
+    queryset = Doctor.objects.prefetch_related('schedules')
     if not specialization == None:
         queryset = Doctor.objects.filter(specialization=specialization)
     return queryset
